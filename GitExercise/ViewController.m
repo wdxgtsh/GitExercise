19a19fe0edf1073e3fc8b5ba5119a756698a356f
zhaolei <zhaolei@allinmd.cn>
2017-06-19 10:16:55 +0800

    create branch feature_1

--- GitExercise/ViewController.m
+++ GitExercise/ViewController.m
@@ -16,6 +16,9 @@
 
 - (void)viewDidLoad {
     [super viewDidLoad];
+    
+    NSLog(@"============>  feature 1 ");
+    
     // Do any additional setup after loading the view, typically from a nib.
 }
 
