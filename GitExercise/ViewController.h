a663a265ae662de5195f4a58dc2c30927d97c42b
zhaolei <zhaolei@allinmd.cn>
2017-06-19 09:31:38 +0800

    init project

---
+++ GitExercise/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  GitExercise
+//
+//  Created by zhaolei on 2017/6/19.
+//  Copyright © 2017年 zhaolei. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
---
+++ GitExerciseTests/GitExerciseTests.m
@@ -0,0 +1,39 @@
+//
+//  GitExerciseTests.m
+//  GitExerciseTests
+//
+//  Created by zhaolei on 2017/6/19.
+//  Copyright © 2017年 zhaolei. All rights reserved.
+//
+
+#import <XCTest/XCTest.h>
+
+@interface GitExerciseTests : XCTestCase
+
+@end
+
+@implementation GitExerciseTests
+
+- (void)setUp {
+    [super setUp];
+    // Put setup code here. This method is called before the invocation of each test method in the class.
+}
+
+- (void)tearDown {
+    // Put teardown code here. This method is called after the invocation of each test method in the class.
+    [super tearDown];
+}
+
+- (void)testExample {
+    // This is an example of a functional test case.
+    // Use XCTAssert and related functions to verify your tests produce the correct results.
+}
+
+- (void)testPerformanceExample {
+    // This is an example of a performance test case.
+    [self measureBlock:^{
+        // Put the code you want to measure the time of here.
+    }];
+}
+
+@end
